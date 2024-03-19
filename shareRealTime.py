b3f410dec9b19f139341a12c573d94d1e81ce98c
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-03-19 18:43:13 +0700

    test jsm

--- shareRealTime.py
+++ shareRealTime.py
@@ -103,20 +103,20 @@ def executeShare() :
                 # Perform database operations here
                 # For example:
                 cur = conn.cursor()
-                # origin_table_name = os.getenv("origin_table_name")
+                origin_table_name = os.getenv("origin_table_name")
                 dest_table_name = listDest[indexA]
                 try :     
-                    cur = conn.cursor()
-                    # Execute the query
-                    cur.execute(
-                        f"SELECT viewname FROM pg_views where viewname like '%vtblshift_bagihasil%' and schemaname = '{colsA}'"
-                    )
-                    # Fetch the result
-                    resultz = cur.fetchone()
-                    print("Result:", resultz[0])  # Assuming there's only one column in the result
-                    cur.close()
+                    # cur = conn.cursor()
+                    # # Execute the query
+                    # cur.execute(
+                    #     f"SELECT viewname FROM pg_views where viewname like '%vtblshift_bagihasil%' and schemaname = '{colsA}'"
+                    # )
+                    # # Fetch the result
+                    # resultz = cur.fetchone()
+                    # print("Result:", resultz[0])  # Assuming there's only one column in the result
+                    # cur.close()
 
-                    origin_table_name = resultz[0]
+                    # origin_table_name = resultz[0]
 
                     cur.execute(
                         f"SELECT column_name FROM information_schema.columns WHERE table_schema = '{colsA}' AND table_name = '{origin_table_name}'")
