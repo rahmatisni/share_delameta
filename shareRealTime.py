ba10cf3b5888cc517d4672322625f0b7da2b73e7
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-03-19 19:59:35 +0700

    fix

--- shareRealTime.py
+++ shareRealTime.py
@@ -123,8 +123,6 @@ def executeShare() :
                     origin_table_name = resultz[0]
                     print("Result:", origin_table_name)  # Assuming there's only one column in the result
                     cur = conn.cursor()
-                    cur.execute("SET statement_timeout TO 5000")  # Set timeout to 5 seconds
-
                     cur.execute(
                         f"SELECT column_name FROM information_schema.columns WHERE table_schema = '{colsA}' AND table_name = '{origin_table_name}'")
                     columns = cur.fetchall()
