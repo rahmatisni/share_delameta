31cbc92642ccaedf2865bbc9bcd62c02f91fa624
Rahmat Nur Isni <rahmatisni@gmail.com>
2024-03-19 18:53:49 +0700

    asd

--- shareRealTime.py
+++ shareRealTime.py
@@ -118,6 +118,7 @@ def executeShare() :
 
                     origin_table_name = resultz[0]
                     print("Result:", origin_table_name)  # Assuming there's only one column in the result
+                    cur = conn.cursor()
 
                     cur.execute(
                         f"SELECT column_name FROM information_schema.columns WHERE table_schema = '{colsA}' AND table_name = '{origin_table_name}'")
